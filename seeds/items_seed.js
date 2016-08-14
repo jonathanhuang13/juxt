d8daa4cc1cbc017a4d80b65ffdbf687e118a925d
jonathanhuang13 <jonathan.huang1@gmail.com>
2016-08-14 16:50:27 -0700

    item seed and update gitignore

---
+++ seeds/items_seed.js
@@ -0,0 +1,29 @@
+import uuid from 'node-uuid';
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('items').del()
+    .then(function () {
+      return Promise.all([
+        // Inserts seed entries
+        knex('items').insert({
+          id: uuid.v1(), 
+          title: 'Chicken breast',
+          brand: 'Tyson\'s',
+          price: 5.99,
+          amount: 4,
+          unit: 'breasts',
+          tags: ['frozen', 'meat']
+        }),
+        knex('items').insert({
+          id: uuid.v1(), 
+          title: 'Strawberry',
+          brand: 'Dole',
+          price: 8.59,
+          amount: 2,
+          unit: 'pint',
+          tags: ['organic', 'fruit']
+        }),
+      ]);
+    });
+};
