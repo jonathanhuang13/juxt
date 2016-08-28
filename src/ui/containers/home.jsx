3f45fcb086adb2cdde7c43e5e7f0e8cd8c4a3b22
jonathanhuang13 <jonathan.huang1@gmail.com>
2016-08-27 22:55:39 -0700

    iteration on home page

--- src/ui/containers/home.jsx
+++ src/ui/containers/home.jsx
@@ -3,30 +3,63 @@ import produceImage from '../assets/images/produce.png';
 import { Button } from './common_components';
 
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { itemsName: null, storesName: null };
+  }
+
+  handleItemsUpdate(event) {
+    this.setState({ itemsName: event.target.value });
+  }
+
+  handleStoresUpdate(event) {
+    this.setState({ storesName: event.target.value });
+  }
+
+  handleSubmit() {
+    console.log(this.state);
+  }
+
   renderTitle() {
-    return (
-      <div>
-        <h1 className='title'>Cheaper groceries, tastier meals</h1>
-        <br />
-        <h3 className='subtitle'>Compare grocery prices across your favorite stores</h3>
-        <br />
-      </div>);
+    return <div>
+      <h1 className='title'>Cheaper groceries, tastier meals</h1>
+      <br />
+      <h3 className='subtitle'>Compare grocery prices across your favorite stores</h3>
+      <br />
+    </div>;
   }
 
   renderForm() {
-    return (
-      <div className='form-group'>
-        { this.renderInputs() }
-        <Button className='btn btn-danger btn-lg'>Search</Button>
-      </div>);
+    return <div className='form-group'>
+      { this.renderInputs() }
+      { this.renderButton() }
+    </div>;
+  }
+
+  renderButton() {
+    return <Button className='btn btn-danger btn-lg' onClick={this.handleSubmit.bind(this)}>Search</Button>;
   }
 
   renderInputs() {
-    return (
-      <div className='inputs'>
-          <input type='text' className='form-control input-lg' placeholder='Item'/>
-          <input type='text' className='form-control input-lg' placeholder='Store'/>
-      </div>);
+    const inputs = [ 'items', 'stores' ];
+
+    return <div className='inputs'>
+        { inputs.map(this.renderInput.bind(this)) }
+    </div>;
+  }
+
+  renderInput(input, i) {
+    const onChange = input === 'items' ? this.handleItemsUpdate.bind(this) : this.handleStoresUpdate.bind(this);
+
+    const params = {
+      type: 'text',
+      className: 'form-control input-lg',
+      placeholder: 'Enter ' + input + ' here',
+      onChange
+    }
+
+    return <input key={i} {...params}/>;
   }
 
   render() {
